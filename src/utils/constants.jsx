28598e698ae21685ab9b357f591c77adcda674e0
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-10 16:27:41 -0300

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,4 @@
-const API_URL = 'https://academia-cds-back-end.vercel.app'
+const API_URL = 'https://academia-cds-back-end.onrender.com'
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/auth/signUp`,
   SIGN_IN: `${API_URL}/auth/signIn`,
