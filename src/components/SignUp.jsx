1cc6a9d4c97a3409fabed1fe9da48682ac75589a
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-16 14:46:35 -0300

    Update SignUp.jsx

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -8,7 +8,7 @@ import CdsSistemasFrente from '../assets/CdsSistemasFrente.jpg';
 
 const SignUp = () => {
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [firstname, setFirstname] = useState('');
@@ -36,7 +36,6 @@ const SignUp = () => {
       });
       if (!response?.data?.token) {
         console.log('Algo deu errado durante o cadstro: ', response);
-        return;
       }
       alert('Cadastro realizado com sucesso!');
       navigate(APP_ROUTES.SIGN_IN);
@@ -46,7 +45,7 @@ const SignUp = () => {
         alert('preencha todos os campos!')
       }
       if (err.response.data.message == 'Este email já está sendo utilizado') {
-        alert('Este email já está sendo utilizado')
+        alert(err.response.data.message)
       }
       else {
         console.log('Ocorreu algum erro no cadastro: ', err.response.data.message);
@@ -68,53 +67,66 @@ const SignUp = () => {
             Cadastre-se
           </h2>
 
-          <form className='flex flex-1 flex-col justify-evenly'>
-            <input
-              className="border-2 outline-none p-2 rounded-md"
-              type="text"
-              placeholder="Nome"
-              value={firstname}
-              required
-              onChange={(e) => { setFirstname(e.target.value); }}
-            />
-            <input
-              className="border-2 outline-none p-2 rounded-md"
-              type="text"
-              placeholder="Sobre Nome"
-              value={lastname}
-              required
-              onChange={(e) => { setLastname(e.target.value); }}
-            />
-            <input
-              className="border-2 outline-none p-2 rounded-md"
-              type="email"
-              placeholder="Digite seu E-mail"
-              value={email}
-              required
-              onChange={(e) => { setEmail(e.target.value); }}
-            />
-            <input
-              className="border-2 outline-none p-2 rounded-md"
-              type="password"
-              placeholder="*******" value={password}
-              required
-              onChange={(e) => { setPassword(e.target.value); }}
-            />
+          <div className="flex flex-1 flex-col justify-evenly items-center">
 
-            <button
-              className="flex justify-center p-2 rounded-md w-2/3 sm:w-1/2 self-center bg-gray-800  text-white hover:bg-gray-700"
-              onClick={signUp}
-            >
-              {
-                isLoading ?
-                  <div className="mr-2 w-5 h-5 border-l-2 rounded-full animate-spin" /> : null
-              }
-              <span>
-                Cadastre-se
-              </span>
-            </button>
-          </form>
+            <form className='space-y-6 w-2/3 items-center'>
+              <div>
+                <input
+                  className="border-2 outline-none p-2 rounded-md w-3/3"
+                  type="text"
+                  placeholder="Nome"
+                  value={firstname}
+                  required
+                  onChange={(e) => { setFirstname(e.target.value); }}
+                />
+              </div>
+              <div>
+                <input
+                  className="border-2 outline-none p-2 rounded-md w-3/3"
+                  type="text"
+                  placeholder="Sobre Nome"
+                  value={lastname}
+                  required
+                  onChange={(e) => { setLastname(e.target.value); }}
+                />
+              </div>
+              <div>
+                <input
+                  className="border-2 outline-none p-2 rounded-md w-3/3 "
+                  type="email"
+                  placeholder="Digite seu E-mail"
+                  value={email}
+                  required
+                  onChange={(e) => { setEmail(e.target.value); }}
+                />
+              </div>
+              <div>
+                <input
+                  className="border-2 outline-none p-2 rounded-md w-3/3"
+                  type="password"
+                  placeholder="*******"
+                  value={password}
+                  required
+                  onChange={(e) => { setPassword(e.target.value); }}
+                />
+              </div>
 
+              <div className="flex flex-col items-center">
+                <button
+                  className="flex justify-center p-2 rounded-md w-1/2 self-center bg-gray-800  text-white hover:bg-gray-700"
+                  onClick={signUp}
+                >
+                  {
+                    isLoading ?
+                      <div className="mr-2 w-5 h-5 border-l-2 rounded-full animate-spin" /> : null
+                  }
+                  <span>
+                    Cadastre-se
+                  </span>
+                </button>
+              </div>
+            </form>
+          </div>
           <div className="text-center text-sm">
             Já é cadastrado?
             <Link to="/signin">
