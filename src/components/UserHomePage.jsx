58ced1f8ba5cda3cf1d26121de106b983009f2fc
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-23 18:42:48 +0000

    update

--- src/components/UserHomePage.jsx
+++ src/components/UserHomePage.jsx
@@ -61,7 +61,7 @@ const UserHomePage = () => {
         <div className="flex flex-col items-center w-full">
           <div className=" sm:text-5xl pb-10">
             <h1 className="text-5xl text-center font-bold tracking-tight text-gray-900">
-              Bem-vindo ao CDS academy, {user.firstname}!
+              Bem-vindo à Academia CDS, {user.firstname}!
             </h1>
           </div>
           <div className=" sm:text-4xl pb-4 w-2/3 text-center pb-8">
