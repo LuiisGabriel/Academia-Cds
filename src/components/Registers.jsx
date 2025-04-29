8b9151ddf6dbf6d9b6627b4b6e409502f286f4d1
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-29 17:28:19 -0300

    Update Registers.jsx

--- src/components/Registers.jsx
+++ src/components/Registers.jsx
@@ -44,7 +44,7 @@ const Registers = () => {
                         Aqui estão disponiveis os cadastros para o CDS Academy!
                     </h1>
                 </div>
-                <div className=" grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8 py-8 px-8">
+                <div className=" grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8 py-8 px-8">
                     {cadastros.map((cadastro) => (
                         <a key={cadastro.id} href={cadastro.href} className="group">
                             <div className="bg-white h-full flex flex-col rounded-lg pb-8 group-hover:scale-102">
