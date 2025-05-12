050e7a8c8ca351782fb02652feb7c276fa0d17d4
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-05-12 16:56:38 -0300

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,14 +3,20 @@ import { useUser } from '../lib/customHooks';
 import Navbar from './Navbar';
 import { useEffect, useState } from 'react';
 import defaultProfilePhoto from '../assets/profile.png';
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 
 const Profile = () => {
   const { user, authenticated } = useUser();
   const [profilePhoto, setProfilePhoto] = useState('');
   const photoUrl = user?.photo?.url;
   const [watchedvideosmesage, setWatchedvideosmesage] = useState();
-  const watchedVideos = user?.watchedVideos?.map((watchedVideo)=>(watchedVideo.id));
-  const isCorrectColor = "#dc2626"
+  const watchedVideos = user?.watchedVideos?.map((watchedVideo) => (watchedVideo.id));
+  const isCorrectColor = "#dc2626";
+  const [open, setOpen] = useState(false);
+  const [valuationId, setValuationId] = useState('');
+  const [valuationTitle, setValuationTitle] = useState('');
+  const [answerOptions, setAnswerOptions] = useState([]);
 
   useEffect(() => {
     if (photoUrl == null) {
@@ -39,17 +45,68 @@ const Profile = () => {
 
   return (
     <>
-
       <div className="w-full h-auto min-h-screen  bg-gray-300">
         <nav className="sticky top-0 z-50"><Navbar /></nav>
         <div className='flex justify-center items-center pt-16'>
+
+
+          <Dialog open={open} onClose={setOpen} className="relative z-10">
+            <DialogBackdrop
+              transition
+              className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+            />
+
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <DialogPanel
+                  transition
+                  className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 w-5/6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+                >
+
+                  <div className='flex justify-end items-end p-4'>
+                    <h1
+                      onClick={() => setOpen(false)}
+                      className='hover:scale-110 cursor-pointer text-lg'>
+                      X
+                    </h1>
+                  </div>
+                  <div className="flex items-start w-full pb-4">
+                    <div className="mt-3 text-center sm:mt-0 sm:text-left px-8 w-full">
+                      <DialogTitle as="h3" className="text-base font-semibold text-gray-900">
+                        {valuationTitle}
+                      </DialogTitle>
+                      <div className="flex flex-col py-4 gap-4">
+                        {answerOptions.map((option, index) => (
+                          <div
+                            key={index}
+                            className="flex items-center justify-between p-2 gap-8 bg-(--color)/70 rounded-lg"
+                            style={{
+                              '--color': option.isCorrect === true ? "#84cc16" : isCorrectColor,
+                            }}
+                          >
+                            <h1>{option.question}</h1>
+                            <h1>{option.isCorrect === true ? "certa" : "errada"}</h1>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+
+                </DialogPanel>
+              </div>
+            </div>
+          </Dialog>
+
+
           <div className="w-4/5 sm:w-8/9 h-full shadow-lg rounded-md bg-white p-8 flex flex-col">
             <div className='flex justify-center items-center'>
-              <img
-                alt={photoUrl}
-                src={profilePhoto}
-                className="size-24 rounded-full"
-              />
+              <div className='flex flex-col justify-center items-end'>
+                <img
+                  alt={photoUrl}
+                  src={profilePhoto}
+                  className="size-24 rounded-full hover:scale-105 hover:opacity-80 transition-all duration-300 ease-in-out"
+                />
+              </div>
             </div>
             <h1 className="text-center font-medium text-2xl ">
               {user.firstname} {user.lastname}
@@ -74,14 +131,18 @@ const Profile = () => {
                   </h1>
                 </div>
                 {user?.answeredValuations?.map((valuation) => (
-                  <div 
-                  key={valuation.valuationId}
-                  className='flex justify-between items-center w-full py-2'>
+                  <div
+                    onClick={() => {
+                      setOpen(true);
+                      setValuationId(valuation.valuationId);
+                      setValuationTitle(valuation.valuationTitle);
+                      setAnswerOptions(valuation.results);
+                    }}
+                    key={valuation.valuationId}
+                    className='flex justify-between items-center w-full py-2'>
                     <div
-                      className="flex items-center justify-between p-4 gap-8 bg-(--color)/80 rounded-lg w-full hover:bg-(--color)/50 hover:scale-101 transition-all duration-300 ease-in-out"
-                      style={{
-                        '--color': valuation.score / valuation.results.length > 0.7 ? "#84cc16" : isCorrectColor,
-                      }}
+                      className="flex items-center justify-between p-4 gap-8 bg-gray-300 rounded-lg w-full hover:scale-101 transition-all duration-300 ease-in-out"
+
                     >
                       <h1 className='font-semibold'>
                         {valuation.valuationTitle}
