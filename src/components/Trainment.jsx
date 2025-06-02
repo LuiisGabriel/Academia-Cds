40366edadf96a902bfe6fc2a6cd2d0919e832796
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-06-02 16:10:07 -0300

    Add files via upload

---
+++ src/components/Trainment.jsx
@@ -0,0 +1,137 @@
+import Navbar from '../components/Navbar';
+import { useUser } from '../lib/customHooks';
+import ReactPlayer from 'react-player';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_ROUTES } from '../utils/constants';
+import { useLocation } from 'react-router-dom';
+
+const Trainment = () => {
+    const { user, authenticated } = useUser();
+    const email = user?.email;
+
+    const location = useLocation();
+    const { ambiente, modulo, subModulo, videos } = location.state || { ambiente: '', modulo: '', subModulo: '', videos: [] };
+
+    const [playedTime, setPlayedTime] = useState(0);
+
+    const initialVideo = videos[0];
+
+    const [watchedVideos, setWatchedVideos] = useState([]);
+    const [videoId, setVideoId] = useState('');
+    const [videoTitulo, setVideoTitulo] = useState('');
+    const [videoDescricao, setVideoDescricao] = useState('');
+    const [videoUrl, setVideoUrl] = useState('');
+
+    useEffect(() => {
+        if (initialVideo) {
+            setVideoId(initialVideo.videoId);
+            setVideoTitulo(initialVideo.titulo);
+            setVideoUrl(initialVideo.url);
+            setVideoDescricao(initialVideo.description);
+        }
+    }, [initialVideo])
+
+    useEffect(() => {
+        if (watchedVideos) {
+            return;
+        }
+    }, [watchedVideos]);
+
+    if (!user || !authenticated) {
+        return (
+            <div className="p-16 bg-gray-300 h-screen flex justify-center items-center">
+                <div className="ml-2 w-8 h-8 border-l-2 rounded-full animate-spin border-white" />
+            </div>
+        );
+    }
+
+    function handleProgress(progress) {
+        if (!progress.seeking) {
+            setPlayedTime(progress.playedSeconds);
+        }
+    }
+
+    const handleEnded = async () => {
+
+        if (user?.watchedVideos?.map((video) => video.videoId).includes(videoId)) {
+            return;
+        }
+
+        const updatedWatchedVideos = [
+            ...watchedVideos,
+            { videoId: videoId, videoTitulo: videoTitulo, videoUrl: videoUrl, videoDescricao: videoDescricao },
+        ]
+        setWatchedVideos(updatedWatchedVideos);
+
+        try {
+            const mergedWatchedVideos = [
+                ...(user.watchedVideos || []),
+                ...updatedWatchedVideos,
+            ];
+
+            const response = await axios.post(API_ROUTES.UPDATE_USER_WATCHED_VIDEOS, {
+                email,
+                watchedVideos: mergedWatchedVideos,
+            });
+
+        } catch (error) {
+            console.error('Error updating watched videos:', error.response?.data || error);
+        }
+    };
+
+    return (
+        <>
+
+            <div className="bg-gray-300 h-auto h-full min-h-screen">
+                <nav className="sticky top-0 z-50"><Navbar /></nav>
+                <div className=' px-8 py-8 justify-center select-none'>
+                    <div className='md:flex gap-8'>
+
+                        <div className="flex flex-col w-full">
+                            <div className="aspect-video" key={videoId}>
+                                <ReactPlayer
+                                    url={videoUrl}
+                                    width="100%"
+                                    height="100%"
+                                    onEnded={handleEnded}
+                                    onProgress={handleProgress}
+                                    controls={true}
+                                />
+                            </div>
+                            <div>
+                                <h3 className="py-4 text-xl">{videoTitulo}</h3>
+                                <h1 className='p-2 text-md'>{videoDescricao}</h1>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center w-full py-8 md:py-0 md:w-2/5">
+                            <div className="flex-col flex w-full gap-4">
+                                {videos?.map((video, index) => (
+                                    <button
+                                        className="bg-gray-800 flex items-center p-2 justify-center rounded-md h-30 hover:bg-gray-700"
+                                        onClick={() => {
+                                            setVideoId(video.videoId);
+                                            setVideoTitulo(video.titulo);
+                                            setVideoUrl(video.url);
+                                            setVideoDescricao(video.description);
+                                        }}
+                                        key={video.id}
+                                    >
+                                        <a className="text-white flex justify-center items-center gap-4">
+                                            <h1>{video.titulo}</h1>
+                                            <h1>{index + 1}</h1>
+                                        </a>
+                                    </button>
+                                ))}
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Trainment;
---
+++ src/components/UserHomePage.jsx
@@ -0,0 +1,83 @@
+import { useUser } from '../lib/customHooks';
+import { APP_ROUTES } from '../utils/constants';
+import Navbar from './Navbar';
+import FrenteDeLoja from '../assets/FrenteDeLoja.jpg';
+import Retaguarda from '../assets/Retaguarda.jpg';
+import Otica from '../assets/Ótica.jpg';
+import Fabrica from '../assets/Fabrica.jpg';
+import PetShop from '../assets/PetShop.jpg';
+import Nfe from '../assets/MóduloNfe.jpg';
+import Light from '../assets/Light.jpg';
+import CdsDesktop from '../assets/CDSDESKTOP.png';
+
+const funcoes = [
+  {
+    id: 1,
+    name: 'Treinamentos',
+    href: APP_ROUTES.TRAINMENTS,
+    imageSrc: Light,
+    imageAlt: 'Treinamentos',
+
+  },
+
+  {
+    id: 2,
+    name: 'Avaliações',
+    href: APP_ROUTES.VALUATIONS,
+    imageSrc: CdsDesktop,
+    imageAlt: 'Avaliações',
+
+  },
+]
+
+const UserHomePage = () => {
+  const { user, authenticated } = useUser();
+
+  if (!user || !authenticated) {
+    return <div className="p-16 bg-gray-300 h-screen flex justify-center items-center">
+      <div className="ml-2 w-8 h-8 border-l-2 rounded-full animate-spin border-white" />
+    </div>;
+  }
+
+  return (
+    <>
+      <div className="h-auto min-h-screen bg-gray-300">
+        <nav className="sticky top-0 z-50"><Navbar /></nav>
+        <div className='flex justify-center p-16 select-none'>
+          <div className="flex flex-col items-center ">
+            <div className=" sm:text-5xl pb-10">
+              <h1 className="text-5xl text-center font-semibold tracking-tight text-gray-900 pb-4 whitespace-pre">
+                Você é {user.firstname} não é?
+              </h1>
+              <h1 className='text-5xl text-center font-bold tracking-tight text-gray-900'>
+                Seja bem-vindo ao CDS Academy!
+              </h1>
+            </div>
+            <div className=" sm:text-4xl pb-4 w-2/3 text-center pb-16">
+              <h1 className="text-3xl font-normal tracking-tight text-gray-700">
+                Aqui você pode aprender sobre o sistema CDS.
+              </h1>
+            </div>
+
+            <div className="flex flex-col shadow-lg rounded-md bg-white p-8 w-3/3">
+              <div className="pb-8">
+                <h1 className="text-2xl font-normal tracking-tight">
+                  acesso rápido:
+                </h1>
+              </div>
+              <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 items-center">
+                {funcoes.map((funcao) => (
+                  <a key={funcao.id} href={funcao.href} className="group">
+                    <img className="aspect-video w-full rounded-lg group-hover:scale-102 transition-all duration-300 ease-in-out group-hover:opacity-75 xl:aspect-video" src={funcao.imageSrc} />
+                    <h3 className="text-center mt-4 text-sm text-gray-700">{funcao.name}</h3>
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default UserHomePage;
