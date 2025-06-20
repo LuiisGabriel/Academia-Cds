adc886efe19b0cc33d5dd61dd614be77a43c75bd
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-06-20 13:41:20 -0300

    Update trainments.jsx

--- src/components/trainments.jsx
+++ src/components/trainments.jsx
@@ -14,8 +14,7 @@ const Trainments = () => {
     const [subModulo, setSubModulo] = useState('');
     const [ambiente, setAmbiente] = useState('');
     const [searchTitle, setSearchTitle] = useState('');
-    const [filters, setFilters] = useState(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-        '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']);
+    const [filters, setFilters] = useState([]);
     const [FilteredTreinamentos, setFilteredTreinamentos] = useState(treinamentos);
 
     const filtros = [
